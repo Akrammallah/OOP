7d425003701a778c2ef6d24426b1dfdf1e99874f
Akrammallah <153257896+Akrammallah@users.noreply.github.com>
2024-06-10 10:09:22 +0500

    OOP

---
+++ index.ts
@@ -0,0 +1,58 @@
+
+import inquirer from "inquirer";
+
+class Student {
+  name:String;
+  constructor(n:string){
+    this.name = n
+  }
+}
+class Person {
+  student:Student[] = []
+enterStudent(obj:Student){
+  this.student.push(obj);
+}  
+}
+const person = new Person()
+
+  
+
+let studentDetail = async (person:Person) => {
+  do {
+  let ans = await inquirer.prompt({
+    name:"select",
+    type:"list",
+    message:"Please enter student name",
+    choices:["Staff","student","Exit"]
+  });
+if(ans.select == "Staff"){
+  console.log("you approach the staff room.Please feel free to ask any question");
+  }else if(ans.select == "student"){
+    let res = await inquirer.prompt({
+      name:"name",
+      type:"input",
+      message:"Enter your name"
+    });
+    const student = person.student.find((val) => val.name == res.name)
+    
+    if(!student){
+      const name = new Student(res.name)
+      person.enterStudent(name)
+      console.log(`hello i am ${name.name} .nice to meet you`);
+      console.log("new student added");
+      console.log("current student list");
+      console.log(person.student);
+   }else{
+    console.log(`hello i am ${student.name} .nice to see you again`);
+    console.log("Exiting student list");
+    console.log(person.student);
+    
+   }
+  }else if (ans.select === "Exit"){
+    console.log("Exiting the program");
+    process.exit();
+    
+  }
+  } while(true);
+ } 
+ studentDetail(person)
